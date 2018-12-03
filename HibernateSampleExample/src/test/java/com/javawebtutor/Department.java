c5fdbc964ea6515da12868add8f7c60467da32f7
ibrahimshbat <ibrahimsabat@yahoo.com>
2018-12-03 23:29:47 +0000

    Add getter and setter

--- HibernateSampleExample/src/test/java/com/javawebtutor/Department.java
+++ HibernateSampleExample/src/test/java/com/javawebtutor/Department.java
@@ -3,6 +3,19 @@ package com.javawebtutor;
 public class Department {
 	
 	private String name;
+
+	public Department(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	
 
